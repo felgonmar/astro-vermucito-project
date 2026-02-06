b5cfea7bbf1456309626c8812626e25e13e5ff36
felgonmar <felixgm1997@gmail.com>
2026-02-06 22:05:09 +0100

    wip first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Vermucito";
+export const SITE_DESCRIPTION = "A refreshing vermut spritz with citrus - The perfect moment in every sip.";
